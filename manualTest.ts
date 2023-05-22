f18ce1264f0b13aac6ba83a9d91b8997425b3219
Andrew Morris <voltrevo@gmail.com>
2023-05-22 13:12:53 +1000

    Move files, clean up manual test

--- manualTest.ts
+++ manualTest.ts
@@ -1,20 +1,4 @@
-// import * as shamir from "./shamir.ts";
-// import FieldElement from "./FieldElement.ts";
-import RecoveryCurve from "./RecoveryCurve.ts";
-
-// const shares = shamir.generate(new FieldElement(123n), 2, 3);
-
-// console.log(
-//   await Promise.all(
-//     shares.map(async (s) =>
-//       `${Number(s.x.n)}: ${(await s.y.toMnemonic()).join(" ")}`
-//     ),
-//   ),
-// );
-
-// const recovered = shamir.recover([shares[0], shares[2]]);
-
-// console.log((await recovered.toMnemonic()).join(" "));
+import RecoveryCurve from "./src/RecoveryCurve.ts";
 
 // const secret =
 //   "expire gun route tornado female reflect holiday grief spring clown deliver army";
@@ -31,7 +15,7 @@ import RecoveryCurve from "./RecoveryCurve.ts";
 const recoveryData = `
 charlotte: valley when dial juice vapor ill payment lunch brass zone alpha topic
 daniel: long neutral reject ahead cart proud shoulder auction april same solar match
-emily: junior coach innocent fragile aware ritual recipe rhythm equal vicious path tray
+emily: come foam country senior awkward task possible auction spice hidden sphere giraffe
 `;
 
 const curve = await RecoveryCurve.fit(
